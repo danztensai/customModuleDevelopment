f9b9b606c8c4cde78e0a8350f93f18dccd415dac
Danial Habibi <danztensai@DanZs-MacBook-Pro.local>
2017-03-14 00:44:26 +0700

    Inherit _prepare_order_line_invoice_line so custom field sale order value can be pass to invoice

--- custom_quotation/models.py
+++ custom_quotation/models.py
@@ -2,7 +2,7 @@
 
 from openerp import models, fields, api
 
-class add_new_field(models.Model):
+class add_new_field_inherit(models.Model):
     #Inhertis the model product.template
     _inherit = 'sale.order.line'
     
@@ -11,6 +11,22 @@ class add_new_field(models.Model):
     Vehicle = fields.Many2one('fleet.vehicle','Vehicle')
     DeliveryDate = fields.Date('Delivery Date')
     DeliveryNumber = fields.Many2one('fleet.work.order','Delivery Order Number')
+
+    def _prepare_order_line_invoice_line(self, cr, uid, line, account_id=False, context=None):
+        ret = super(add_new_field_inherit, self)._prepare_order_line_invoice_line(cr, uid, line, account_id=False, context=context)
+        if line.product_id: 
+            if line.Destination:
+                ret['Destination'] = line.Destination
+            if line.DeliveryOrder:
+                ret['DeliveryOrder'] = line.DeliveryOrder
+            if line.Vehicle:
+                ret['Vehicle'] = line.Vehicle
+            if line.DeliveryNumber:
+                ret['DeliveryNumber'] = line.DeliveryNumber
+            if line.DeliveryDate:
+                ret['DeliveryDate'] = line.DeliveryDate
+
+        return ret 
 
 class add_new_field(models.Model):
     #Inhertis the model product.template
